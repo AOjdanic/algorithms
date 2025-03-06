0c54f27ec8425b14c0095e1b7f109559728a07c6
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-03-06 23:45:40 +0100

    improve performance of the fibonacci recursion function with memoization

--- fibonacci.ts
+++ fibonacci.ts
@@ -1,8 +1,33 @@
 function fibonacci(n: number) {
+  if (n < 0) return 0;
   if (n === 0) return 0;
   if (n === 1) return 1;
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(30));
+const cache: Record<number, number> = {};
+
+function fibonacciMemoized(n: number) {
+  if (n < 0) return 0;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  if (cache[n]) return cache[n];
+
+  cache[n] = fibonacciMemoized(n - 1) + fibonacciMemoized(n - 2);
+  return cache[n];
+}
+
+// console.time("fibonacci");
+// console.log(fibonacci(50));
+// console.timeEnd("fibonacci");
+
+// fibonacci: 1:38.321 (m:ss.mmm)
+
+console.time("fibonacciMemoized");
+console.log(fibonacciMemoized(50));
+console.timeEnd("fibonacciMemoized");
+
+// fibonacciMemoized: 3.469ms -> for 50
+// fibonacciMemoized: 3.398ms -> for 200
